6ac8fa139bd4a1214de6e0cf55c04c9cc28ee69b
RO0MM <sotorommel22@gmail.com>
2026-05-07 14:50:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 // 3) Usa la llave "anon public". NO uses la llave "service_role".
 
 window.INNOV_CHAT_CONFIG = {
-  SUPABASE_URL: 'https://TU-PROYECTO.supabase.co',
-  SUPABASE_ANON_KEY: 'TU_ANON_KEY_PUBLICA'
+  SUPABASE_URL: 'https://rfjvskrimsoqlyofhidj.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_P1cgsqPGMWHHWIujJayyqg_9hgJEXZh'
 };
